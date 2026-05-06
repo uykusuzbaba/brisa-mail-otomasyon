e1afb682b511f29ab1ba9be0eacbf7a136987f97
uykusuzbaba <ramsesium.md@gmail.com>
2026-05-06 12:52:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -82,6 +82,7 @@ RAKIP_FIRMALAR = [
     "BIRLA CARBON",       # BIRLA CARBON EGYPT S.AE
     "UNIMAC",             # UNIMAC RUBBER CO.,LTD.
     "LANXESS",            # LANXESS BELGIUM NV
+    "ATOMEX",             # ATOMEX SIA
 ]
 
 # Eleme limanları (POL = Port of Loading)
